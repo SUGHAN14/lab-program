f6bb087912ff1d611d07c6963fcea7accc87cfb3
SUGHAN <118059255+SUGHAN14@users.noreply.github.com>
2024-02-05 14:11:36 +0530

    Create bfs.py

---
+++ bfs.py
@@ -0,0 +1,27 @@
+graph = {
+    '5': ['3', '7'],
+    '3': ['2', '4'],
+    '7': ['8'],
+    '2': [],
+    '4': ['8'],
+    '8': []
+}
+
+visited = []  # Use a set for efficiency
+queue = []
+
+def bfs(visited, graph, node):
+    visited.append(node)  # Use add() for sets
+    queue.append(node)
+
+    while queue:
+        current_node = queue.pop(0)
+        print(current_node, end=" ")
+
+        for neighbour in graph[current_node]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+print("Following is the breadth-first search:")
+bfs(visited, graph, '5')
